d67f7fcb30af33d391379e458ae2a9d14e2a234e
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2023-09-06 22:10:33 +0100

    sever setup, list implementation as well as add task feature

---
+++ main.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser"
+
+let app = express()
+const port = 3000
+
+app.use(express.static("static"))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let list = []
+
+app.get("/", (req,res) => {
+    res.render("index.ejs")
+})
+
+app.post("/", (req,res) => {
+    list.push(req.body["text"])
+    res.render("index.ejs", {list: list, listlength: list.length})
+})
+
+app.listen(port, function() {
+    console.log("[INFO] Server is up and running in port " + port)
+})
